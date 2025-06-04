574f6102561f568bfc1a3a2f7265905944f7f065
Arnold <mainaarnold8350@gmail.com>
2025-06-04 09:14:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName = 'scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+function setCompanyName(scuber) {
+    it('is set as Scuber', function () {
+        expect(companyName).to.equal('Scuber');
+    }
+}
